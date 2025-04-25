e3c29d7e821411c7805a7304e982989d00a06a54
PedraoGrigolato <pedrohenriquegrigolato3@gmail.com>
2025-04-25 12:28:37 -0300

    desenvolvimento do código

--- GalloShoppingList/app/index.js
+++ GalloShoppingList/app/index.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {link} from 'expo-router'
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Aôôba !</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Image source={require('../assets/avatar.jpg')}
+         style={styles.avatar}
+        />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Lista de compras braba!
+        </Text>
+        <Text style={styles.Text}>
+          Não esqueça mais suas compras com esse aplicativo sensacional!
+        </Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}>Acessar"</Text>
+        </Link>
+      </View>
+
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -13,8 +30,50 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  containerImage: {
+    flex: 2,
+    alignItems:'center',
     justifyContent: 'center',
   },
+  avatar: {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#dadada',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: 'beige',
+  },
+  button: {
+    position: 'absolute',
+    bottom: '15%',
+    backgroundColor: '#000',
+    alignSelf: 'center',
+    borderRadius: 50,
+    paddingVertical: 15,
+    width:'60%',
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
 });
