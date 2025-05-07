8c12704a1e7b9154b1e6ea85879643735da9254c
PedraoGrigolato <pedrohenriquegrigolato3@gmail.com>
2025-05-07 13:59:11 -0300

    Desenvolvimento dos códigos

--- GalloShoppingList/app/home.jsx
+++ GalloShoppingList/app/home.jsx
@@ -1,12 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={ {flex: 1 } }>
+      <ImageBackground 
+      source={require('../assets/background.jpg')}
+      style={ {flex: 1,justifyContent: 'flex-start'}}
+      resizeMode='repeat'
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}> Lista de Produtos</Text>
+          <Ionicons name="trash" size={32} color="#fff" />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({ 
+  header: {
+    padding: 25,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#000000c0',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#fff'
+  }
+})
